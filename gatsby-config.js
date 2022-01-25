793f5db133fbd26c78fa4c4c1aeba8a3934c2bfd
fernandobosco <fbosco@dlocal.com>
2022-01-25 19:13:43 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+      title: `new_blog`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "LW07W1OYrt8hJXFF2W5Yda-Yxs0v2CS0I3Y8ejNk5es",
+      "spaceId": "2hy6h0qlqpdj"
+    }
+  }, "gatsby-plugin-postcss", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-54GKT4GLFS"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
